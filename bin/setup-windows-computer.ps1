374e5c64d93221c06f92dd0c0e2b33fd1257e47b
Marco Chomut <marco.chomut@gmail.com>
2022-03-02 10:39:22 -0500

    Dump of windows setup script changes for personal

--- bin/setup-windows-computer.ps1
+++ bin/setup-windows-computer.ps1
@@ -2,11 +2,9 @@
 # Install-BoxstarterPackage -PackageName <this-script-url> -DisableReboots
 
 # TODO: do some verification of the boxstarter install script before running it
+# (Open PowerShell as Administrator)
+# Set-ExecutionPolicy Bypass -Scope Process -Force
 # . { iwr -useb https://boxstarter.org/bootstrapper.ps1 } | iex; Get-Boxstarter -Force
-
-# Once WinGet supports zip files and other installation methods, chocolatey can
-# likely be retired, but until then there are still a few items we require from
-# it.
 
 # Install winget
 $hasPackageManager = Get-AppPackage -name 'Microsoft.DesktopAppInstaller'
@@ -32,206 +30,327 @@ Enable-MicrosoftUpdate
 Install-WindowsUpdate -Full -acceptEula
 
 Disable-BingSearch
-Enable-RemoteDesktop
+# Enable-RemoteDesktop  # TODO: broken on 11?
 Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions -EnableShowFullPathInTitleBar -EnableOpenFileExplorerToQuickAccess -EnableShowRecentFilesInQuickAccess -EnableShowFrequentFoldersInQuickAccess -EnableExpandToOpenFolder -EnableShowRibbon
-Set-BoxstarterTaskbarOptions -Dock Left
+# TODO: Boxstarter Dock Left may be broken on Win11
+# Set-BoxstarterTaskbarOptions -Dock Left
 
-# Set-ExecutionPolicy Bypass -Scope Process -Force
 # Set-ExecutionPolicy Unrestricted -Scope CurrentUser
 
-# TODO: remove built-in bloatware like TikTok and CandyCrush
+# TODO: remove built-in bloatware:
+# TikTok, CandyCrush, Sotify, Solitare, Disney+, Clipchamp, Prime Video, Instagram, Facebook
+# some may be added back later based on profile, but definitely don't have them by default
+# TODO: some of these, like Spotify, should not be installed from an Admin prompt?
 
 # Exit  # TODO: for testing
 
+$wapps = {@()}.Invoke()
+# Once WinGet supports zip files and other installation methods, chocolatey can
+# likely be retired, but until then there are still a few items we require from
+# it.
+$capps = {@()}.Invoke()
+
+# TODO: lots of apps in here, like SublimeText, whose install names don't match their list names later on when we query for them, causing them to be re-installed over and over
+
 # Core:
-winget install --exact Google.Chrome
-winget install --exact Mozilla.Firefox
-winget install --exact Google.Drive
-winget install --exact 7zip.7zip
-winget install --exact VideoLAN.VLC
-winget install --exact Microsoft.Teams  # TODO: is this included in office?
-winget install --exact Microsoft.Office
-winget install --exact Zoom.Zoom
-winget install --exact Malwarebytes.Malwarebytes
-winget install --exact Foxit.FoxitReader  # TODO: this or adobereader?
+$wapps.Add(@{name = "Google.Chrome"})
+$wapps.Add(@{name = "Mozilla.Firefox"})
+$wapps.Add(@{name = "Google.Drive"})
+$wapps.Add(@{name = "7zip.7zip"})
+$wapps.Add(@{name = "VideoLAN.VLC"})
+$wapps.Add(@{name = "Microsoft.Teams"})
+$wapps.Add(@{name = "Microsoft.Office"})
+$wapps.Add(@{name = "Zoom.Zoom"})
+$wapps.Add(@{name = "Malwarebytes.Malwarebytes"})
+$wapps.Add(@{name = "Foxit.FoxitReader"})  # TODO: this or adobereader?
 # pdfcreator
-cinst paint.net  # not yet on winget
-winget install --exact TimKosse.FileZilla.Client
-winget install --exact Apple.iTunes
-winget install --exact CodecGuide.K-LiteCodecPack.Full
-winget install --exact Spotify.Spotify
-winget install --exact Dropbox.Dropbox
-winget install --exact SlackTechnologies.Slack
-winget install --exact voidtools.Everything
-winget install --exact AntibodySoftware.WizMouse
-winget install --exact AntibodySoftware.WizTree
-winget install --exact QL-Win.QuickLook
-winget install --exact Streamlink.Streamlink
-winget install --exact Streamlink.Streamlink.TwitchGui
+$capps.Add("paint.net")
+$wapps.Add(@{name = "TimKosse.FileZilla.Client"})
+$wapps.Add(@{name = "Apple.iTunes"})
+$wapps.Add(@{name = "CodecGuide.K-LiteCodecPack.Full"})
+$wapps.Add(@{name = "Spotify.Spotify"})
+$wapps.Add(@{name = "Dropbox.Dropbox"})
+$wapps.Add(@{name = "SlackTechnologies.Slack"})
+$wapps.Add(@{name = "voidtools.Everything"})
+# TODO: this may not be necessary on Win11 now?
+# winget install --exact AntibodySoftware.WizMouse
+$wapps.Add(@{name = "AntibodySoftware.WizTree"})
+$wapps.Add(@{name = "QL-Win.QuickLook"})
+$wapps.Add(@{name = "Streamlink.Streamlink"})
+$wapps.Add(@{name = "Streamlink.Streamlink.TwitchGui"})
 # TODO: chatterino
 # winget install --exact Twitch.Twitch
-# TODO: only pick one of these three
-winget install --exact GPSoftware.DirectoryOpus
-# winget install --exact Ghisler.TotalCommander
-# winget install --exact alexx2000.DoubleCommander
-# TODO: only pick one of these two
-winget install --exact Wox.Wox
-# winget install --exact OliverSchwendener.ueli
+$wapps.Add(@{name = "GPSoftware.DirectoryOpus"})
+# TODO: decide if PowerToys Run is good enough
+$wapps.Add(@{name = "OliverSchwendener.ueli"})
 # things
 # twitter client
 # vmware remote console
 # flow
 # adium
-# game streams
 # icloud
 # jre
 # netflix
 # driver sweeper
-winget install --exact IDRIX.VeraCrypt
-winget install --exact Element.Element
-winget install --exact Cockos.LICEcap
-winget install --exact Resilio.ResilioSync
-winget install --exact TorProject.TorBrowser
-winget install --exact WireGuard.WireGuard
-winget install --exact AgileBits.1Password
-winget install --exact Anki.Anki
-winget install --exact DelugeTeam.Deluge
-winget install --exact flux.flux
-winget install --exact HandBrake.HandBrake
-winget install --exact Microsoft.MouseandKeyboardCenter
-winget install --exact Microsoft.MouseWithoutBorders
-winget install --exact PeterBClements.QuickPar
-winget install --exact Piriform.Defraggler
-winget install --exact Piriform.Recuva
-winget install --exact RevoUninstaller.RevoUninstaller
-winget install --exact VB-Audio.Voicemeeter.Banana
+$wapps.Add(@{name = "IDRIX.VeraCrypt"})
+$wapps.Add(@{name = "Element.Element"})
+$wapps.Add(@{name = "Cockos.LICEcap"})
+$wapps.Add(@{name = "Resilio.ResilioSync"})
+$wapps.Add(@{name = "TorProject.TorBrowser"})
+# vpn
+# backups
+$wapps.Add(@{name = "WireGuard.WireGuard"})
+# using choco's 1password for v7.x
+$capps.Add("1password")
+# $wapps.Add(@{name = "AgileBits.1Password"})
+$wapps.Add(@{name = "Anki.Anki"})
+$wapps.Add(@{name = "DelugeTeam.Deluge"})
+$wapps.Add(@{name = "flux.flux"})
+$wapps.Add(@{name = "HandBrake.HandBrake"})
+$wapps.Add(@{name = "Microsoft.MouseWithoutBorders"})
+$wapps.Add(@{name = "PeterBClements.QuickPar"})
+$wapps.Add(@{name = "Piriform.Defraggler"})
+$wapps.Add(@{name = "Piriform.Recuva"})
+$wapps.Add(@{name = "RevoUninstaller.RevoUninstaller"})
+$wapps.Add(@{name = "Netflix"; id = "9WZDNCRFJ3TJ"; source = "msstore"})
 
 # Artistry:
-winget install --exact Inkscape.Inkscape
-winget install --exact Audacity.Audacity
-winget install --exact BlenderFoundation.Blender
-winget install --exact KDE.Krita
-winget install --exact darktable.darktable
-winget install --exact Figma.Figma
-winget install --exact MiXXX.MiXXX
-winget install --exact Meltytech.Shotcut
-winget install --exact Autodesk.sketchbook
+$wapps.Add(@{name = "Inkscape.Inkscape"})
+$wapps.Add(@{name = "Audacity.Audacity"})
+$wapps.Add(@{name = "BlenderFoundation.Blender"})
+$wapps.Add(@{name = "KDE.Krita"})
+$wapps.Add(@{name = "darktable.darktable"})
+$wapps.Add(@{name = "Figma.Figma"})
+$wapps.Add(@{name = "MiXXX.MiXXX"})
+$wapps.Add(@{name = "Meltytech.Shotcut"})
+$wapps.Add(@{name = "Autodesk.sketchbook"})
 # natron
-# obs studio
+$wapps.Add(@{name = "OBSProject.OBSStudio"})
+$wapps.Add(@{name = "VB-Audio.Voicemeeter.Banana"})
+$wapps.Add(@{name = "Cockos.REAPER"})
 # davinci resolve
 
 # Dev Tools:
-winget install --exact Notepad++.NotePad++
+$wapps.Add(@{name = "Notepad++.NotePad++"})
 # TODO: which dotnet runtime/compiler to get? Microsoft.dotnet?
-winget install --exact Microsoft.PowerShell
-cinst powershell-core  # not yet on winget
-winget install --exact Sysinternals.Suite --source msstore  # TODO: name is likely wrong
-winget install --exact Lexikos.AutoHotkey
-winget install --exact Microsoft.VisualStudioCode
+$wapps.Add(@{name = "Microsoft.PowerShell"})
+$capps.Add("powershell-core")
+$wapps.Add(@{name = "Sysinternals Suite"; id = "9P7KNL5RWT25"; source = "msstore"})
+$wapps.Add(@{name = "Lexikos.AutoHotkey"})
+$wapps.Add(@{name = "Microsoft.VisualStudioCode"})
 # cinst winlogbeat
-winget install --exact Microsoft.WindowsTerminal
-winget install --exact WiresharkFoundation.Wireshark
-winget install --exact Oracle.Virtualbox
+$wapps.Add(@{name = "Microsoft.WindowsTerminal"})
+$wapps.Add(@{name = "WiresharkFoundation.Wireshark"})
+$wapps.Add(@{name = "Oracle.Virtualbox"})
 # cinst virtualbox.extensionpack
-winget install --exact Microsoft.PowerToys
-winget install --exact Docker.DockerDesktop
-cinst cmder  # not yet on winget
-winget install --exact SublimeHQ.SublimeMerge
-winget install --exact SublimeHQ.SublimeText.4
-cinst starship  # not yet on winget
-winget install --exact geardog.gsudo
-winget install --exact WinMerge.WinMerge
-winget install --exact Insomnia.Insomnia
-winget install --exact Insomnia.InsomniaDesigner
-winget install --exact Microsoft.WinDbg --source msstore
-winget install --exact Google.AndroidStudio
-winget install --exact Hashicorp.Vagrant
-winget install --exact PuTTY.PuTTY
-winget install --exact RealVNC.VNCViewer
-winget install --exact WinMerge.WinMerge
-winget install --exact WinSCP.WinSCP
+$wapps.Add(@{name = "Microsoft.PowerToys"})
+$wapps.Add(@{name = "Docker.DockerDesktop"})
+$capps.Add("cmder")
+$wapps.Add(@{name = "SublimeHQ.SublimeMerge"})
+$wapps.Add(@{name = "SublimeHQ.SublimeText.4"})
+$capps.Add("starship")
+$wapps.Add(@{name = "gerardog.gsudo"})
+$wapps.Add(@{name = "WinMerge.WinMerge"})
+$wapps.Add(@{name = "Insomnia.Insomnia"})
+$wapps.Add(@{name = "Insomnia.InsomniaDesigner"})
+$wapps.Add(@{name = "WinDbg Preview"; id = "9PGJGD53TN86"; source = "msstore"})
+$wapps.Add(@{name = "Google.AndroidStudio"})
+$wapps.Add(@{name = "Hashicorp.Vagrant"})
+$wapps.Add(@{name = "PuTTY.PuTTY"})
+$wapps.Add(@{name = "RealVNC.VNCViewer"})
+$wapps.Add(@{name = "WinSCP.WinSCP"})
 # TODO: eventually when need .net dev, evaluate: codetrack, linqpad, nuget package explorer, fusion+
-# TODO: repoz
-# godot
+$wapps.Add(@{name = "AndreasWascher.RepoZ"})
+$capps.Add("godot")
+$wapps.Add(@{name = "UnityTechnologies.Unity.2021"})
 # dash/zeal
 # ghidra
 # vmware workstation
 # Some tools are duplicated in Windows and WSL, due to their need in native development
-# git
+$wapps.Add(@{name = "Git.Git"})
 # jdk
 # python
 
 # WSL:
 # see available distros with: wsl --list --online
-wsl --install -d Debian  # TODO: arch?
-wsl --set-default-version 2
-wsl --setdefault Debian
-# TODO: setup initial username/password besides root
+# TODO: idempotence check
+# wsl --install -d Debian  # TODO: arch?
+# wsl --set-default-version 2
+# wsl --setdefault Debian
+# TODO: setup initial username/password besides root, window will prompt after install
 
 # Gaming:
-cinst gamesavemanager
-winget install --exact NexusMods.Vortex
-winget install --exact Discord.Discord
+$capps.Add("gamesavemanager")
+$wapps.Add(@{name = "NexusMods.Vortex"})
+$wapps.Add(@{name = "Discord.Discord"})
 # TODO: is emulationstation a frontend or does it include all the emulators?
 # winget install --exact DolphinEmulator.Dolphin
-winget install --exact Valve.Steam
-winget install --exact GOG.Galaxy
-winget install --exact Blizzard.BattleNet  # TODO: this may be msstore
-cinst retroarch
-# cinst logitechgaming
-# winget install --exact Logitech.Options
-winget install --exact Emulationstation.Emulationstation
-winget install --exact Codeusa.BorderlessGaming
-winget install --exact EpicGames.EpicGamesLauncher
-winget install --exact ElectronicArts.EADesktop
-winget install --exact Ubisoft.Connect
+$wapps.Add(@{name = "Valve.Steam"})
+$wapps.Add(@{name = "GOG.Galaxy"})
+$capps.Add("battle.net")
+$capps.Add("retroarch")
+$wapps.Add(@{name = "Emulationstation.Emulationstation"})
+$wapps.Add(@{name = "Codeusa.BorderlessGaming"})
+$wapps.Add(@{name = "EpicGames.EpicGamesLauncher"})
+$wapps.Add(@{name = "ElectronicArts.EADesktop"})
+$wapps.Add(@{name = "Ubisoft.Connect"})
 # rockstar launcher
-cinst legendary
+$capps.Add("legendary")
 # TODO: valorant, legends of runeterra, riot client?
-winget install --exact OBSProject.OBSStudio
-# xbox
-# xbox game pass
-# xbox smartglass 360/one
+$wapps.Add(@{name = "Xbox"; id = "9MV0B5HZVK9Z"; source = "msstore"})
+$wapps.Add(@{name = "Xbox Console Companion"; id = "9WZDNCRFJBD8"; source = "msstore"})
+# xbox game bar should come by default
 # guild wars 2
 # samsung hmd odyssey+ setup
-# uplay, ,
-# logitech gaming
 # zam minion
 # gamecompanion
-winget install --exact FlawlessWidescreen.FlawlessWidescreen
-winget install --exact LOOT.LOOT  # TODO: boss?
-winget install --exact Mumble.Mumble
-winget install --exact PlayStation.PSRemotePlay
+# playnite
+$wapps.Add(@{name = "FlawlessWidescreen.FlawlessWidescreen"})
+$wapps.Add(@{name = "LOOT.LOOT"})  # TODO: boss?
+$wapps.Add(@{name = "Mumble.Mumble"})
+$wapps.Add(@{name = "PlayStation.PSRemotePlay"})
+$wapps.Add(@{name = "Mixed Reality Portal"; id = "9NG1H8B3ZC7M"; source = "msstore"})
+# HMD Odyssey Plus?
+$wapps.Add(@{name = "Xbox Accessories"; id = "9NBLGGH30XJ3"; source = "msstore"})
 
 # Performance:
-cinst 3dmark
-cinst pcmark8
+# $capps.Add("3dmark")
+# $capps.Add("pcmark8")
 # userbenchmark
 # memtest86
 # thaiphoon burner
-# cinebench
-cinst twitch-bandwidth-tester
+$capps.Add("cinebench")
+$capps.Add("twitch-bandwidth-tester")
 # winget install --exact CPUID.CPU-Z
-winget install --exact CPUID.CPU-Z.MSI
-winget install --exact CPUID.HWMonitor
-winget install --exact TechPowerUp.GPU-Z
+$wapps.Add(@{name = "CPUID.CPU-Z.MSI"})
+$wapps.Add(@{name = "CPUID.HWMonitor"})
+$wapps.Add(@{name = "TechPowerUp.GPU-Z"})
+$capps.Add("speedfan")
 
 # Build Specific:
-# msi afterburner
+$capps.Add("msiafterburner")
 # ryzen master
-winget install --exact RyzenControllerTeam.RyzenController
-# msi center (older: dragon center)
+$capps.Add("amd-ryzen-chipset")
+$wapps.Add(@{name = "RyzenControllerTeam.RyzenController"})
+$wapps.Add(@{name = "MSI Center"; id = "9NVMNJCR03XV"; source = "msstore"})
 # trident z lighting control
 # l-connect
-winget install --exact NZXT.CAM
-cinst nvidia-display-driver
-winget install --exact Nvidia.GeForceExperience
-# logitech capture
+$wapps.Add(@{name = "NZXT.CAM"})
+$capps.Add("nvidia-display-driver")
+$wapps.Add(@{name = "Nvidia.GeForceExperience"})
+$wapps.Add(@{name = "Logitech.GHUB"})
 # astro command center
 # avermedia recentral
+$wapps.Add(@{name = "Microsoft.MouseandKeyboardCenter"})
+# nahimic from msstore
+# realtek audio control from msstore
+# winget install --exact Logitech.Options  # TODO: for builds with MX Master
+# Logitech G500S still not available from G HUB
+$capps.Add("logitechgaming")
+# TODO: change to
+# $wapps.Add(@{name = "Logitech.LGS"})
+
+Foreach ($app in $wapps) {
+    $id = $app.name
+    if ($app.id -ne $null) {
+        $WingetArgs = @("--id", $app.id)
+    } else {
+        $WingetArgs = @("--exact", $app.name)
+    }
+
+    $listText = winget list --accept-source-agreements --query $id --exact
+    if ([String]::Join("", $listText).Contains($id)) {
+        continue
+    }
+
+    if ($app.source -ne $null) {
+        winget install @WingetArgs --source $app.source --accept-source-agreements --accept-package-agreements
+    } else {
+        winget install @WingetArgs --accept-source-agreements --accept-package-agreements
+    }
+}
+
+# $listText = choco list --local
+$listText = clist --local
+Foreach ($app in $capps) {
+    if ([String]::Join("", $listText).Contains($app)) {
+        continue
+    }
+    cinst --cacheLocation="c:\temp" --yes $app
+    # depending on the formula, may have to resort to manual checks and some combo of:
+    # --ignore-checksum --allow-empty-checksums --allow-empty-checksums-secure
+}
 
 # winget upgrade
 # winget upgrade -all
 # winget import -i <list.json>
+# cup all
+# choco upgrade all
 
 Enable-UAC
+
+# After success:
+# Restart-Computer
+
+# Manual Configuration:
+
+# TODO:
+# Add the following to the end of ~\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 'Invoke-Expression (&starship init powershell)'
+
+# BIOS
+# CPU Overclocking > SVM ON
+
+# L-Connect
+
+# f.lux
+# Enter zip code
+
+# Docker Desktop
+# TODO: must enable hardware assisted virt and DEP in BIOS
+
+# GOG Galaxy
+# Settings > General > Uncheck Launch at system startup
+
+# Discord
+# Settings > Windows Settings > Uncheck Open Discord
+
+# Nvidia Control Panel
+# Gsync in both fullscreen and windowed mode
+
+# MSI Center
+# Feature Sets: Mystic Light
+
+# Epic Games
+# https://old.reddit.com/r/EpicGamesPC/comments/ie63nl/how_to_detect_existing_installations_of_game/gbhr1x7/
+
+# Settings
+
+# System
+# Activate now
+
+# Display
+# Arrange monitor layout
+# Set primary monitor to 144 hz refresh rate
+
+# Personalization
+# Taskbar
+# Uncheck Show my taskbar on all displays
+# TODO: can't do above until 11 allows taskbar to be moved, so until then:
+# When using multiple displays, show my taskbar apps on: All taskbars
+
+# Desktop App Groups/Folders:
+# - Game
+# - Design
+# - Develop
+# - Control
+# - Talk
+# - Listen
+# - Watch
+# - Work
+# - Share
+# - Look Up
+# - Configure
+# - Utilities
+
+# Taskbar Pins:
+# Start, Search, Desktops, Talk, Explorer, Store, Firefox, Terminal, Code
